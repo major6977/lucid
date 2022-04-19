d9aaa1890bcf682e8a44c8d967fa1229cddbeb1e
major davis <majordav@msn.com>
2022-04-18 17:04:35 -0700

    added functions and change id name in css

--- lucid.js
+++ lucid.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function() {
+    $("#underline-movement").mouseover(function() {
+        $("#actual-underline").animate({width: "105px"});
+    })
+});
+$(document).ready(function() {
+    $("#underline-movement").mouseout(function() {
+        $("#actual-underline").animate({width: "0"});
+    })
+});
