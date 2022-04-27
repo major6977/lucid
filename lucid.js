81fa9e33c7a994bd48bf19e75f3a8bbc605e1d94
major davis <majordav@msn.com>
2022-04-26 18:28:24 -0700

    changed imgs and added stying to new cont

--- lucid.js
+++ lucid.js
@@ -1,10 +1,10 @@
-$(document).ready(function() {
-    $("#underline-movement").mouseover(function() {
-        $("#actual-underline").animate({width: "105px"});
-    })
+$(document).ready(function () {
+  $("#underline-movement").mouseover(function () {
+    $("#actual-underline").animate({ width: "105px" });
+  });
 });
-$(document).ready(function() {
-    $("#underline-movement").mouseout(function() {
-        $("#actual-underline").animate({width: "0"});
-    })
+$(document).ready(function () {
+  $("#underline-movement").mouseout(function () {
+    $("#actual-underline").animate({ width: "0px" });
+  });
 });
