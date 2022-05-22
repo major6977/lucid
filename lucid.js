ce696b6224ceb5e481ac5e6a2fa16396fc6a92cb
major davis <majordav@msn.com>
2022-05-22 12:25:42 -0700

    added mobile menu functions and styled

--- lucid.js
+++ lucid.js
@@ -4,3 +4,17 @@ $(window).scroll(function () {
   $("#fade-txt1").css("opacity", 1.7 - $(window).scrollTop() / 500);
   $("#fade-txt2").css("opacity", 1.7 - $(window).scrollTop() / 800);
 });
+
+// Hamburger Bar Menu
+
+function myFunction() {
+  var x = document.getElementById("my-links");
+  var mobileHeader = document.querySelector(".mobile-header");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    mobileHeader.style.height = "40px";
+  } else {
+    x.style.display = "block";
+    mobileHeader.style.height = "250px";
+  }
+}
