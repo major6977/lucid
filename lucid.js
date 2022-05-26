0679ebfde73fc22c909bec63057e4999e6b8456c
major davis <majordav@msn.com>
2022-05-26 11:54:59 -0700

    add dropdown menu to contact page, added id's to images

--- lucid.js
+++ lucid.js
@@ -18,3 +18,22 @@ function myFunction() {
     mobileHeader.style.height = "250px";
   }
 }
+
+// Button Function
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
