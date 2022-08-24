3ae1097c96730e00fda05201e98851cc83e61c31
major davis <majordav@msn.com>
2022-08-24 13:34:55 -0700

    correct functions,changed styles on index and features page

--- lucid.js
+++ lucid.js
@@ -1,12 +1,3 @@
-// Fading Text 1 (Lucid Air Page) and Text 2 (Features Page)
-
-$(window).scroll(function () {
-  $("#fade-txt1").css("opacity", 1.7 - $(window).scrollTop() / 500);
-  $("#fade-txt2").css("opacity", 1.7 - $(window).scrollTop() / 800);
-  $(".fade-txt3").css("opacity", 1.7 - $(window).scrollTop() / 2100);
-  $(".fade-txt-index-pg").css("opacity", 1.7 - $(window).scrollTop() / 1900);
-});
-
 // Hamburger Bar Menu
 
 // function myFunction() {
